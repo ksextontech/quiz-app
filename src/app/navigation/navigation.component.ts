10c18286866247ac3b959f5e7250ac569a0faa50
Kyle Sexton <ksextontech@live.com>
2017-12-27 14:15:32 -0600

    life cycle hooks

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+  ViewChild,
+  ElementRef,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -6,22 +20,71 @@ import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef } from '@an
   styleUrls: ['./navigation.component.css'],
   encapsulation: ViewEncapsulation.Emulated // the default
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements
+OnInit,
+OnChanges,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy {
 
   @ViewChild('mainNav') navElement: ElementRef;
+  @Input() value: string;
 
   constructor() {
-
+    // console.log('constructor())');
   }
 
-  ngOnInit() {
-    console.log(this.navElement);
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log('ngOnChanges()');
+    // console.log(changes);
+    // called once during component creation
+    // and after each time a bound property changes
+  }
+
+  ngOnInit(): void {
+    // console.log('ngOnInit()');
+    // called once the component is initialized
+    // this is called after the constructor
+
+    // console.log(this.navElement);
 
     const nativeElement = this.navElement.nativeElement;
-    console.log(nativeElement);
+    // console.log(nativeElement);
 
     // don't change the element through this, angular has a better way of accessing / manipulating the DOM.
     // nativeElement.innerHTML = '';
   }
 
+  ngDoCheck(): void {
+    // console.log('ngDoCheck()');
+    // called during every change detection run
+  }
+
+  ngAfterContentInit(): void {
+    // console.log('ngAfterContentInit()');
+    // called after (ng-content) has been projected into the view
+  }
+
+  ngAfterContentChecked(): void {
+    // console.log('ngAfterContentChecked()');
+    // called every time the projected content has been checked
+  }
+
+  ngAfterViewInit(): void {
+    // console.log('ngAfterViewInit()');
+    // called after a component's view (and child views) have been initialized
+  }
+
+  ngAfterViewChecked(): void {
+    // console.log('ngAfterViewChecked()');
+    // called every time the view (and child views) have been checked
+  }
+
+  ngOnDestroy(): void {
+    // console.log('ngOnDestroy()');
+    // called once the component is about to be destroyed
+  }
 }
--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ContentChild, ElementRef, AfterViewInit, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
-export class PanelComponent implements OnInit {
+export class PanelComponent implements OnInit, AfterViewInit, AfterContentInit {
   @Input() heading: string;
+  @ViewChild('header') header: ElementRef;
+  @ContentChild('submitButton') submitButton: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    // view child template reference is not available until after the view has been initialized
+    // console.log(this.header.nativeElement.textContent);
+
+    // content child template reference is actually available here
+    // as long as the template reference has been provided with the content injection
+    // console.log(this.submitButton.nativeElement.textContent);
+  }
+
+  ngAfterViewInit(): void {
+    // console.log(this.header.nativeElement.textContent);
+  }
+
+  ngAfterContentInit(): void {
+    // console.log(this.submitButton.nativeElement.textContent);
   }
 
 }
